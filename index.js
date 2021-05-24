e67b7338c5d12982e5b1567d3616337854cf4a4c
wuhongsong <wuhongsong@haier.com>
2021-05-24 08:57:55 +0800

    init

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
+
+// const fs = require('fs')
+// const path = require('path')
+
 class SvgSpriteLoadByDemand {
   constructor(options){
     this.options = options
   }
   apply(compiler){
-    compiler.hooks.assetEmitted.tap("svgSprite",(file, { content, source, outputPath, compilation, targetPath }) => {
-      console.log('svgsrpite ==> end')
-      // console.log(content);
-      // console.log(content.toString()); 
-    });
+
+    // compiler.hooks.thisCompilation.tap('svgSpritePlugin', (compilation)=>{
+    //   compilation.hooks.processAssets.tap('assets',(assets)=>{
+    //     console.log(assets)
+    //   })
+    // })
+
+    // compiler.hooks.watchRun.tapAsync('svgSpritePlugin',(compiler, callback)=>{
+    //   // optimization:
+    //   console.log(compiler.module)
+    // })
+    // compiler.hooks.run.tap('run', () => {
+    //   console.log('开始编译...')
+    // })
+
+    // compiler.hooks.compile.tap('compile', () => {
+    //   console.log('===> compile')
+    // })
+
+    // compiler.hooks.done.tap('compilation', () => {
+    //   console.log('===> compilation')
+    // })
   }
 }
+
+// console.log('file==>',findAllDir(path.resolve("./test/assets/")))
+
+// function findAllDir(filePath) {
+//   const stack = [filePath];
+//   console.log('stack',stack)
+//   const res = [];
+//   while (stack.length > 0) {
+//     const _path = stack.shift();
+//     const files = fs.readdirSync(_path);
+//     files.forEach(function(filename) {
+//       const _filePath = path.join(_path, filename);
+//       const stats = fs.statSync(_filePath);
+//       if (stats.isDirectory()) {
+//         stack.unshift(_filePath);
+//       } else if (stats.isFile() && isSvg(_filePath)) {
+//         res.push(_path);
+//       }
+//     });
+//   }
+//   return res.slice(1);
+// }
+
+// function isSvg(filePath) {
+//   return /svg/g.test(path.extname(filePath));
+// }
 
 
 module.exports = SvgSpriteLoadByDemand
--- loader/index.js
+++ loader/index.js
@@ -1,13 +1,20 @@
+// const mixer = require('svg-mixer');
 const mapSvgJson = {}
 
-module.exports = function(source){
+module.exports = function(source,map, meta){
   console.log('=======loader==========')
-  const loaderContext = this;
+  // const loaderContext = this;
+  // var callback = this.async();
+  // let filePath = loaderContext.resourcePath.split(loaderContext.query.path)[0]
+  // console.log(filePath)
+  // console.log(mapSvgJson)
+  // let json = {}
+  // console.log(loaderContext.resourcePath)
+  // callback(null, `module.exports = ${JSON.stringify(json)}`, map, meta);
 
-  console.log(this.data)
-
-  return source;
-  // return source;
+  // return `module.exports = ${JSON.stringify(mapSvgJson)}`
+  return source
+  
 }
 
 module.exports.pitch = function(remainingRequest, precedingRequest, data){
@@ -15,15 +22,16 @@ module.exports.pitch = function(remainingRequest, precedingRequest, data){
   // data.value = 'aa'
   // console.log(this.resource)
   // console.log(this.query)
-  let splitPath = this.query.path + '/'
-  let curPathArr = this.resource.split(splitPath)
-  let curSvg = curPathArr.length ? curPathArr[1].split('/') : []
-  console.log(curSvg)
-  // mapSvgJson[curSvg.split('/')
-  mapSvgJson[curSvg[0]] ? mapSvgJson[curSvg[0]].push(curSvg[1]) : mapSvgJson[curSvg[0]] = [curSvg[1]]
-  console.log('mapSvgJson',mapSvgJson)
-  data.value = mapSvgJson
+  // let splitPath = this.query.path + '/'
+  // let curPathArr = this.resource.split(splitPath)
+  // let curSvg = curPathArr.length ? curPathArr[1].split('/') : []
+  // // console.log(curSvg)
+  // // mapSvgJson[curSvg.split('/')
+  // mapSvgJson[curSvg[0]] ? mapSvgJson[curSvg[0]].push(this.resource) : mapSvgJson[curSvg[0]] = [this.resource]
+  // // console.log('mapSvgJson',mapSvgJson)
+  // data.value = mapSvgJson
   // console.log('remainingRequest', remainingRequest)
   // console.log('precedingRequest',precedingRequest)
-  console.log('===pitch===')
+  // console.log('===pitch===')
+  
 }
