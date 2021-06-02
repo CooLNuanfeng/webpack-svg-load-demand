744e672dfbfba9c29ea70323aa89cee26958c7d1
wuhongsong <wuhongsong@haier.com>
2021-06-02 17:02:16 +0800

    debug

--- plugin-webpack4.js
+++ plugin-webpack4.js
@@ -12,37 +12,42 @@ class SvgSpriteLoadByDemand {
       compilation.hooks.optimizeChunkAssets.tapAsync('SvgSpriteLoadByDemand',
         (chunks, callback) => {
           chunks.forEach(chunk => {
-
             chunk.files.forEach(file => {
               let content = new ConcatSource(compilation.assets[file]).source();
-              let fsPath = path.resolve(compiler.options.context, this.options.entryRoot)
-              let spriter = new SVGSpriter({
-                mode: {
-                  symbol: true
-                }
-              });
               var reg = /\\"svg-path\\": \\"(.*?)\\"/g
-              let svgFiles = content.match(reg) || []
-              let appendContent = ''
-              svgFiles.forEach(item => {
-                var pathJson = JSON.parse(`{${item.replace(/\\/g,'')}}`);
-                spriter.add(path.resolve(fsPath, pathJson['svg-path']), null, fs.readFileSync(path.resolve(fsPath, pathJson['svg-path']), {encoding: 'utf-8'}));
-              });
-              // console.log(compilation.assets[file])
-              spriter.compile(function(error, result) {
-                if(error) console.log(error)
-                for (var mode in result) {
-                  for (var resource in result[mode]) {
-                    appendContent = `;(function(){
-                      document.getElementById('_svg_sprites_demand_wrap_') && document.getElementById('_svg_sprites_demand_wrap_').remove()
-                      document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend','<div style="display:none" id="_svg_sprites_demand_wrap_">${result[mode][resource].contents.toString()}</div>')
-                    })();`
+              if(reg.test(content)){
+                let fsPath = path.resolve(compiler.options.context, this.options.entryRoot)
+                let spriter = new SVGSpriter({
+                  mode: {
+                    symbol: true
                   }
-                }
-              
-                compilation.assets[file] = new ConcatSource(content,appendContent)
+                });
+                
+                let svgFiles = content.match(reg) || []
+                let appendContent = ''
+                svgFiles.forEach(item => {
+                  var pathJson = JSON.parse(`{${item.replace(/\\/g,'')}}`);
+                  spriter.add(path.resolve(fsPath, pathJson['svg-path']), null, fs.readFileSync(path.resolve(fsPath, pathJson['svg-path']), {encoding: 'utf-8'}));
+                });
+                // console.log(compilation.assets[file])
+                spriter.compile(function(error, result) {
+                  if(error) console.log(error)
+                  for (var mode in result) {
+                    for (var resource in result[mode]) {
+                      //document.getElementById('_svg_sprites_demand_wrap_') && document.getElementById('_svg_sprites_demand_wrap_').remove()
+                      appendContent = `;(function(){
+                        document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend','<div style="display:none">${result[mode][resource].contents.toString()}</div>')
+                      })();`
+                    }
+                  }
+                
+                  compilation.assets[file] = new ConcatSource(content,appendContent)
+                  callback()
+                });
+              }else{
                 callback()
-              });
+              }
+              
             });
 
           });
--- plugin-webpack5.js
+++ plugin-webpack5.js
@@ -17,42 +17,44 @@ class SvgSpriteLoadByDemand {
           let fsPath = path.resolve(compiler.options.context, this.options.entryRoot)
           Object.entries(assets).forEach(([pathname, source]) => {
             // console.log(pathname)
-            let content = source.source()
-            let spriter = new SVGSpriter({
-              mode: {
-                symbol: true
-              }
-            });
             var reg = /\\"svg-path\\": \\"(.*?)\\"/g
-       
-            let svgFiles = content.match(reg) || []
-            let appendContent = ''
-            svgFiles.forEach(item => {
-              var pathJson = JSON.parse(`{${item.replace(/\\/g,'')}}`);
-
-              spriter.add(path.resolve(fsPath, pathJson['svg-path']), null, fs.readFileSync(path.resolve(fsPath, pathJson['svg-path']), {encoding: 'utf-8'}));
-            });
-        
-            spriter.compile(function(error, result) {
-              if(error) console.log(error)
-              for (var mode in result) {
-                for (var resource in result[mode]) {
-                  appendContent = `;(function(){
-                    document.getElementById('_svg_sprites_demand_wrap_') && document.getElementById('_svg_sprites_demand_wrap_').remove()
-                    document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend','<div style="display:none" id="_svg_sprites_demand_wrap_">${result[mode][resource].contents.toString()}</div>')
-                  })();`
+            let content = source.source()
+            if(reg.test(content)){
+              let spriter = new SVGSpriter({
+                mode: {
+                  symbol: true
                 }
+              });
+         
+              let svgFiles = content.match(reg) || []
+              let appendContent = ''
+              svgFiles.forEach(item => {
+                var pathJson = JSON.parse(`{${item.replace(/\\/g,'')}}`);
+  
+                spriter.add(path.resolve(fsPath, pathJson['svg-path']), null, fs.readFileSync(path.resolve(fsPath, pathJson['svg-path']), {encoding: 'utf-8'}));
+              });
+          
+              spriter.compile(function(error, result) {
+                if(error) console.log(error)
+                for (var mode in result) {
+                  for (var resource in result[mode]) {
+                    //document.getElementById('_svg_sprites_demand_wrap_') && document.getElementById('_svg_sprites_demand_wrap_').remove()
+                    appendContent = `;(function(){
+                      document.getElementsByTagName('body')[0].insertAdjacentHTML('beforeend','<div style="display:none">${result[mode][resource].contents.toString()}</div>')
+                    })();`
+                  }
+                }
+              });
+              compilation.assets[pathname] = {
+                // 返回文件内容
+                source: () => {
+                  return content + appendContent
+                },
+                // 返回文件大小
+                size: () => {
+                  return Buffer.byteLength(content + appendContent, 'utf8')
+                },
               }
-            });
-            compilation.assets[pathname] = {
-              // 返回文件内容
-              source: () => {
-                return content + appendContent
-              },
-              // 返回文件大小
-              size: () => {
-                return Buffer.byteLength(content + appendContent, 'utf8')
-              },
             }
           });
         }
